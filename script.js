26920b321922e6cbd555af02f015fec38774c7bc
wuchiachen1212 <wuchiachen1212@yahoo.com.tw>
2017-07-15 15:08:10 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("script.js");
+//
+//function showMsg(msg1,msg2){
+//    alert(msg1+"---"+msg2);
+//}
+
+var showMsg=function(msg1,msg2){
+    var msgAll=    msg1+"---"+msg2;
+                                
+    return msgAll;
+}
